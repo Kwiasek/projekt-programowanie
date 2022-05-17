ff19af2167732966af7a5dbf82a413102b9d7d69
kwiasek <kwiasekkwiasek@gmail.com>
2022-05-17 15:36:45 +0200

    Added page loader and form

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let czas = data.getHours();
 var dataHook = document.querySelector('#czas');
 const hook = document.querySelector('#hook')
 
+
+// Funkcja sprawdza czy w localStorage jest wpis 'imie', jeżeli tak wypisuje je w wyniku, jeżeli nie tworzy przycisk. Po kliknięciu przycisku wyskakuje prompt którego wartość zapisywana jest w localStorage.imie 
 function imie(){
 	var imie;
 	if (!imie){
@@ -41,6 +43,7 @@ function imie(){
 	}
 }
 
+// Funkcja pobiera czas, sprawdza która jest godzina i printuje 'Dzień dobry' lub 'Dobry wieczór' zależnie od godziny 
 function ustalCzas(){
 	if (czas >= 6 && czas <= 19) {
 		dataHook.innerHTML = 'Dzień dobry'
@@ -48,6 +51,7 @@ function ustalCzas(){
 	else {
 		dataHook.innerHTML = 'Dobry wieczór'
 	}
+	showPage();
 	imie();
 	czcionka();
 	pokazSlajdow();
@@ -55,7 +59,7 @@ function ustalCzas(){
 }
 
 
-
+// Funkcja nasłuchuje element '<select>'. Po zmianie wartości funkcja przypisuje wartość do zmiennej :root zmieniając kolorystykę strony 
 function zmienKolor(){
 	var kolorSelect = document.querySelector('#kolor');
 	var kolor = window.sessionStorage.getItem('kolor');
@@ -91,6 +95,7 @@ function zmienKolor(){
 	document.documentElement.style.setProperty('--clr-primary', kolor);
 }
 
+// Funkcja nasłuchuje zmiany w checkboxach i zmienia styl strony zależnie od zaznaczonego checkboxa 
 function czcionka(){
 	var czcionka = document.querySelector('#czcionka');
 	czcionka.addEventListener('change', function(){
@@ -115,20 +120,44 @@ function czcionka(){
 	},false)
 }
 
+// Funkcja losuje wartosc z tablicy oraz zamienia ja w :root dokumentu 
 function pokazSlajdow(){
 	var images = ['url(img/background.jpg)', 'url(img/kontakt.jpg)', 'url(img/olscustoms.jpg)', 'url(img/pic1.jpg)', 'url(img/pic2.jpg)', 'url(img/pic3.jpg)', 'url(img/serwis.jpg)', 'url(img/stacja.jpg)']
 	document.documentElement.style.setProperty('--header-img', images[parseInt(Math.random()*7)])
 }
 setInterval(pokazSlajdow, 2000);
 
-
+// Funckja losuje lewy margines od krawędzi strony i przypisuje go samochodzikowi
 function anim(){
 	var car = document.querySelector('#car')
-	var numer = Math.floor((Math.random()*1500)+ 50);
+	var numer = Math.floor((Math.random()*800)+ 50);
 	car.style.left = numer + 'px';
 }
 setInterval(anim, 30)
 
+function showPage(){
+	setTimeout(showPage, 3000)
+}
+
+function showPage(){
+	document.querySelector('#loader').style.display = 'none';
+	document.querySelector('#page').style.display = 'block';
+
+}
+
+document.querySelector('#oblicz').addEventListener('click', Oblicz, false);
+
+// Funkcja po kliknięciu przycisku pobiera wartość z formularza, mnoży wartość oraz wyświetla ją w paragrafie o id wynik 
+function Oblicz(){
+	var x = document.querySelector('#obliczenie').value;
+	var wynik = document.querySelector('#wynik');
+	if (isNaN(x)){
+		alert('To nie jest liczba')
+	}
+	else {
+		wynik.innerHTML = x * x;
+	}
+}
 
 window.addEventListener('load', ustalCzas, false);
 
